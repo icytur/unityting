d3bf285b7eb0bcb95b08b302ee4016778843a218
icytur <icytur5@gmail.com>
2024-12-02 01:14:59 -0600

    UI
    UI animations added movement fixed. working chest and inventory is being added shortly, an hour maybe

--- knight/Assets/scripts/InventoryManager.cs
+++ knight/Assets/scripts/InventoryManager.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class InventoryManager : MonoBehaviour
 {
     public GameObject inventoryPanel; // Reference to the Inventory Panel
-
+    private Animator animator;        // Animator for the Inventory Panel
     private bool isInventoryOpen = false; // Tracks if the inventory is open
 
-    // Toggles the inventory panel visibility
+    void Start()
+    {
+        animator = inventoryPanel.GetComponent<Animator>();
+    }
+
     public void ToggleInventory()
     {
         isInventoryOpen = !isInventoryOpen;
-        inventoryPanel.SetActive(isInventoryOpen);
+
+        if (isInventoryOpen)
+        {
+            inventoryPanel.SetActive(true);
+            animator.Play("OpenInventory");
+        }
+        else
+        {
+            animator.Play("CloseInventory");
+            StartCoroutine(HidePanelAfterAnimation());
+        }
+    }
+
+    private IEnumerator HidePanelAfterAnimation()
+    {
+        yield return new WaitForSeconds(0.3f); // Match the animation duration
+        inventoryPanel.SetActive(false);
     }
 }
